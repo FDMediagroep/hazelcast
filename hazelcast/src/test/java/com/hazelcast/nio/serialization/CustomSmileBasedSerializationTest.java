4a27ce1fd5bde605ccf8ab3260e120f77be62344
Rene Avontuur <rene.avontuur@fdmediagroep.nl>
2018-07-20 16:21:52 +0200

    added tests showing compatibility of multiple versions of the same class

--- hazelcast/src/test/java/com/hazelcast/nio/serialization/CustomSmileBasedSerializationTest.java
+++ hazelcast/src/test/java/com/hazelcast/nio/serialization/CustomSmileBasedSerializationTest.java
@@ -16,6 +16,7 @@
 
 package com.hazelcast.nio.serialization;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.smile.SmileFactory;
 import com.hazelcast.config.SerializationConfig;
@@ -31,6 +32,7 @@ import org.junit.runner.RunWith;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 @RunWith(HazelcastSerialClassRunner.class)
 @Category(QuickTest.class)
@@ -80,9 +82,41 @@ public class CustomSmileBasedSerializationTest {
         assertEquals(newFoo2, foo);
     }
 
+    @Test
+    public void testSerializerVersioning() {
+        SerializationConfig config = new SerializationConfig();
+        config.setUseNativeByteOrder(false);
+        SerializerConfig sc = new SerializerConfig()
+                .setImplementation(new FooSmileSerializer())
+                .setTypeClass(Foo.class);
+        config.addSerializerConfig(sc);
+        SerializationService ss = new DefaultSerializationServiceBuilder().setConfig(config).build();
+
+        Foo foo = new Foo("f");
+        foo.setName("name");
+        Data d = ss.toData(foo);
+        Foo newFoo = ss.toObject(d);
+
+        assertEquals(newFoo, foo);
+
+        SerializationConfig config2 = new SerializationConfig();
+        config2.setUseNativeByteOrder(false);
+        SerializerConfig sc2 = new SerializerConfig()
+                .setImplementation(new Foo2SmileSerializer())
+                .setTypeClass(Foo2.class);
+        config2.addSerializerConfig(sc2);
+        SerializationService ss2 = new DefaultSerializationServiceBuilder().setConfig(config2).build();
+
+        Foo2 newFoo2 = ss2.toObject(d);
+        assertEquals("f", newFoo2.foo);
+        assertNull(newFoo2.getTitle());
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Foo {
 
         private String foo;
+        private String name;
 
         public Foo() {
         }
@@ -97,6 +131,14 @@ public class CustomSmileBasedSerializationTest {
 
         public void setFoo(String foo) {
             this.foo = foo;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
         }
 
         @Override
@@ -138,6 +180,81 @@ public class CustomSmileBasedSerializationTest {
         @Override
         public Foo read(byte[] bytes) throws IOException {
             return mapper.readValue(bytes, Foo.class);
+        }
+
+        @Override
+        public void destroy() {
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Foo2 {
+
+        private String foo;
+        private String title;
+
+        public Foo2() {
+        }
+
+        public Foo2(String foo) {
+            this.foo = foo;
+        }
+
+        public String getFoo() {
+            return foo;
+        }
+
+        public void setFoo(String foo) {
+            this.foo = foo;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            Foo foo1 = (Foo) o;
+
+            return !(foo != null ? !foo.equals(foo1.foo) : foo1.foo != null);
+
+        }
+
+        @Override
+        public int hashCode() {
+            return foo != null ? foo.hashCode() : 0;
+        }
+    }
+
+    public static class Foo2SmileSerializer implements ByteArraySerializer<Foo2> {
+
+        private SmileFactory f = new SmileFactory();
+        private ObjectMapper mapper = new ObjectMapper(f);
+
+        @Override
+        public int getTypeId() {
+            return 10;
+        }
+
+        @Override
+        public byte[] write(Foo2 foo) throws IOException {
+            return mapper.writeValueAsBytes(foo);
+        }
+
+        @Override
+        public Foo2 read(byte[] bytes) throws IOException {
+            return mapper.readValue(bytes, Foo2.class);
         }
 
         @Override
